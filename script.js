baf5a2604be8f3274b41127f3cb3dab7fd6728e6
giuseppe-eustaquio <gseppe222@gmail.com>
2022-09-21 02:00:18 +0800

    Adds picture in picture function and completes the project

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log("Hello world!");
+// get elements
+
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// functions
+
+// prompt the user to select a media stream, pass to video element, then play
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // catch error here
+    console.log("whoops error", error);
+  }
+}
+
+// add event listeners
+
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+  //   start picture in picture
+
+  await videoElement.requestPictureInPicture();
+
+  // reset button
+  button.disabled = false;
+});
+
+// on load
+
+selectMediaStream();
